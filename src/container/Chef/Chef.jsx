8372d8895138fdbebe7d627dc8e49676b08deed7
Onyenso Precious <chinoprecious29@gmail.com>
2025-03-23 07:02:46 -0700

    complete site

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
 
 import './Chef.css';
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className='app__bg app__wrapper section__padding'>
+    <div className="app__wrapper_img app__wraper_img-reverse">
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className="app__wrapper_info">
+      <SubHeading title="chef's word"/>
+      <h1 className='headtext__cormorant'> what we believe in </h1>
+
+      <div className="app__chef-content">
+        <div className="app__chef-content-quote">
+          <img src={images.quote} alt="quote" />
+          <p className="p__opensans">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <p className="p__opensans">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi blanditiis, placeat repellendus accusantium impedit aliquam repellat modi officiis asperiores similique, </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>kevin luo</p>
+        <p className="p__opensans">chef and founder </p>
+        <img src={images.sign} alt="sign" />
+      </div>
+
+    </div>
   </div>
 );
 
--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,10 +1,47 @@
 import React from 'react';
 
+import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+
+import { FooterOverlay, Newsletter } from '../../components';
 import './Footer.css';
+import { images } from '../../constants';
 
 const Footer = () => (
-  <div>
-    Footer
+  <div className='app__footer section__padding app__bg'>
+    <FooterOverlay />
+    <Newsletter />
+
+    <div className="app__footer-links">
+      <div className="app__footer-links_contact">
+        <h1 className="app__footer-headtext">Contact Us</h1>
+        <p className="p__opensans">No 5 mbuakara off elionparnwa, Nigeria </p>
+        <p className="p__opensans">+243 818 200 7216 </p>
+        <p className="p__opensans">+231 816 229 5944 </p>
+      </div>
+      <div className="app__footer-links_logo">
+        <img src={images.gericht} alt="footer logo" />
+        <p className="p__opensans">the best way to find your self is to lose yourself in the service of others.</p>
+        <img src={images.spoon} alt="spoon" style={{margin: 15}} />
+        <div className="app__footer-links_icons">
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
+      </div>
+      <div className="app__footer-links_work">
+        <h1 className="app__footer-headtext">Workin Hours</h1>
+        <p className="p__opensans">Monday - Friday </p>
+        <p className="p__opensans">08:00 am - 12:00 am </p>
+        <p className="p__opensans">Saturday - Sunday </p>
+        <p className="p__opensans">07:00 am - 11:00 pm</p>
+
+      </div>
+    </div>
+    
+    <div className="footer__copyright">
+      <p className="p__opensans">2025 Stephen. All Right reserved.</p>
+    </div>
+
   </div>
 );
 
